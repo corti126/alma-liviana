21dc6585b31c0fa9828cc2b7765ca3c12766e321
Santiago <santitati1204@gmail.com>
2026-05-28 01:33:58 -0300

    build: position index.html at root for native vite workflow

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  root: './', // Le clava la raíz del proyecto a Vite de forma obligatoria
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), // Le da la ruta absoluta real a Rollup
-      },
-    },
-  },
 })
